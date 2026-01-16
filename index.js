9f53f965cdea3ee5e3a159d6aef3d5566e254b17
S-Arafin <arafin23103@gmail.com>
2026-01-17 02:05:49 +0600

    add products api with dynamic product route for showing details

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
+app.use(cors({
     origin: "http://localhost:3000",
     credentials: true,
-  })
-);
+}));
 app.use(express.json());
 
 const uri = process.env.MONGODB_URI;
+
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function run() {
     try {
         await client.connect();
-        
         await client.db("admin").command({ ping: 1 });
         console.log("GadgetGo DB Connected Successfully!");
+
+        const db = client.db("gadget_go_db");
+        const productsCollection = db.collection("products");
 
         app.get('/', (req, res) => {
             res.send('GadgetGo Server is Running');
         });
 
+        app.get('/products', async (req, res) => {
+            const products = await productsCollection.find().toArray();
+            res.send(products);
+        });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        });
 
     } catch (error) {
         console.error("Connection failed:", error);
